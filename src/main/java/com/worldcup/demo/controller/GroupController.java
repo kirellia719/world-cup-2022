5fea31dd5acb8c9f52b35887af53a7736bd73d4c
khanh.phamhong@hcmut.edu.vn <khanh.phamhong@hcmut.edu.vn>
2022-11-20 11:59:26 +0700

    add CORS

--- src/main/java/com/worldcup/demo/controller/GroupController.java
+++ src/main/java/com/worldcup/demo/controller/GroupController.java
@@ -1,17 +1,21 @@
 package com.worldcup.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.worldcup.demo.service.GroupService;
 
 @RestController
+@CrossOrigin
+@RequestMapping(value = "/group")
 public class GroupController {
     @Autowired
     GroupService groupService;
 
-    @GetMapping(value = "/group")
+    @GetMapping
     public Object getAllGroup(){
         return groupService.getAllGroup();
     }
--- src/main/java/com/worldcup/demo/controller/MatchController.java
+++ src/main/java/com/worldcup/demo/controller/MatchController.java
@@ -1,6 +1,7 @@
 package com.worldcup.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.worldcup.demo.service.MatchService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/match")
 public class MatchController {
     @Autowired
