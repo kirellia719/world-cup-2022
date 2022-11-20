932415fcfd9870dfbd41c5c49b1885a17fb67dcd
khanh.phamhong@hcmut.edu.vn <khanh.phamhong@hcmut.edu.vn>
2022-11-20 11:52:50 +0700

    add CORS

--- src/main/java/com/worldcup/demo/DemoApplication.java
+++ src/main/java/com/worldcup/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DemoApplication {
 
 	public static void main(String[] args) {
